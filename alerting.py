c98e51ecf0faacd943d518f0264ecf8c5732e887
rjsears <richard@sears.net>
2016-05-30 19:55:33 -0700

    Alerting include file

---
+++ alerting.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+# Set up some alerting so we know when the pool is getting filled up and when it is done.
+PoolAlerting = "True"
+alertEmail = "your@email.here" #Email address to include in log files. No email actually sent. 
+pushbilletAPI = "lkdjl;dkjlkjdflkjdfl;gkjdflkjgdflk" # pushbullet API token (http://www.pushbullet.com)
