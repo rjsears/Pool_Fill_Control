a34950ac25cea04a0401c22e87feb3b5344b9fd3
rjsears <richard@sears.net>
2016-06-05 22:54:32 -0700

    Comments updated for Watchdog support

--- pooldb.py
+++ pooldb.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
 ## Setup our Database Connection
-servername = "localhost"
+servername = "haarp.rstechnical.com"
 username = "pool_control" # Your main MySQL admin username or username that is the exact same for both databases
-password = "SomeSecretPasswordHere" # Your main MySQL admin password or password that is exact same for both databases
+password = "4c71v473!" # Your main MySQL admin password or password that is exact same for both databases
 emoncms_db = "emoncms" # The name of your emoncms (or other) database.
 
 
 # Used for Sprinkler Bypass
 sprinkler_bypass = "Yes"
 sprinkler_type = "Rachio"	# Timer or Rachio
-rachio_url = 'curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer jhdjhf-8738783473-kfkshfks-9374873487" https://api.rach.io/1/public/device/59f8edckjshkdhsk-7/current_schedule'
+rachio_url = 'curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer d11d9c4a-6d67-43ef-ad92-9ac9b922f77c" https://api.rach.io/1/public/device/59f8edcb-9149-421a-bbd0-8a7fb8f696b7/current_schedule'
 
 ############################################################################################################################
 ########## Runtime checking and maximum fill times
@@ -21,6 +21,11 @@ checktime = 60		# How often (in seconds) do we want to check the pool level? Thi
 			# like 5 hours unless you want a flooded yard!
 			
 			# Each "checktime cycle" will occur at the interval you specify here.		
+			
+#######  VERY VERY VERY IMPORTANT ####
+# We now support a watchdog timer. I have set the timer for 70 seconds based on this checktime. If you change this checktime
+# you MUST MUST MUST change the timer in /etc/systemd/system/pool_control.service otherwise your script will restart every
+# 70 seconds!!!
 
 maxruntime = 180	# What is the maximum number of checktime cycles you want to let your pool fill? For example
 			# if you put 60 seconds above for checktime, and 180 here, it will be 60 seconds * 180 readings
@@ -35,7 +40,7 @@ maxruntime = 180	# What is the maximum number of checktime cycles you want to le
 
 ## Set the MAXIMUM idle wattage of your pool pump. If the reading is over this wattage, the pool will not fill since the
 ## pump would be running
-max_wattage = 30
+max_wattage = 30 
 
 
 
