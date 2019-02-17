8ebde1cd2f1b038d2e5e3ddc090e5a7115406e9e
rjsears <richard@sears.net>
2019-02-16 18:27:03 -0700

    Create pump_control_software_start.sh

---
+++ pump_control_software_start.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+## For use with pool_control_master V3.5.0
+
+cd /var/www/pump_control_software/nodejs-poolController-5.1.1
+pm2 start /var/www/pump_control_software/nodejs-poolController-5.1.1
+#pm2 start 0
