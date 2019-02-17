1993e8e924ccd57dcfa3977131673a1f074a9489
rjsears <richard@sears.net>
2019-02-16 18:06:16 -0700

    V3.5.0 Update

--- pooldb.py
+++ pooldb.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 
+# Set systemwide loglevel here:
+# INFO
+# WARN
+# DEBUG
+loglevel = "INFO"
+
+__author__ = 'Richard J. Sears'
+VERSION = "V3.5.0 (2019-02-16)"
+# richard@sears.net
+
+
 """ Config file for Pool_Fill_Control"""
-""" V3.03.05 (2018-02-27) """
-
-## Set Notification Types
-## No longer set here - see pool_sensor_status file instead.
-#DEBUG = True
-#LOGGING = True
-#EMAIL = True
-#PUSHBULLET = True
-
-alert_email = 'richard@sears.net'
-twilio_from = '+18585551212'
-twilio_account = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-twilio_token = 'kjsdhfkjshdkfjhksdhuhifudsjhskfj'
-twilio_to = '+18582224444'
+check_url = 'www.google.com'
 
 # Set DB here to read pump watts (only) from DB via emoncms or
 # set NODEJS to use Russ Goldin's nodeJS Pump control software
@@ -24,28 +22,38 @@ PUMP_DATA_URL = "http://localhost:3000/pump"
 PUMP_DATA_TEST_URL = "localhost:3000"
 PUMP_START_URL = "http://localhost:3000/pumpCommand/run/pump/1"
 PUMP_STOP_URL = "http://localhost:3000/pumpCommand/off/pump/1"
+PUMP_PROGRAM1_URL = "http://localhost:3000/pumpCommand/run/pump/1/program/1"
+PUMP_PROGRAM2_URL = "http://localhost:3000/pumpCommand/run/pump/1/program/2"
+PUMP_PROGRAM3_URL = "http://localhost:3000/pumpCommand/run/pump/1/program/3"
+PUMP_PROGRAM4_URL = "http://localhost:3000/pumpCommand/run/pump/1/program/4"
 
-
-## Setup our Database Connection
-servername = "localhost"
-username = "pool_control"  # Your main MySQL admin username or username that is the exact same for both databases
-#username = "pool_control"  # Your main MySQL admin username or username that is the exact same for both databases
-password = "LKhhHuHkvNM"  # Your main MySQL admin password or password that is exact same for both databases
-emoncms_db = "emoncms"  # The name of your emoncms (or other) database.
-
-# Set up some alerting so we know when the pool is getting filled up and when it is done.
-PoolAlerting = "False"
-pushbilletAPI = "o.HkUyiugka8iuyysgskutd7987987"  # pushbullet API token (http://www.pushbullet.com)
 
 ## Pool temperature Sensor Settings
-pool_temp_table = "feed_274"
-pool_temp_sensor_battery_table = "feed_151"
+pool_temp_table = "feed_53"
+pool_temp_sensor_temp_table = "feed_54"
+pool_temp_sensor_battery_table = "feed_52"
+pool_temp_sensor_humidity_table = "feed_55"
 pool_temp_sensor_low_voltage = 2.9
 max_pool_temp_sensor_time_delta = 3000
 
 ## What is the table name for our pool_level feed and other settings for our pool_level_sensor?
-pool_level_table = "feed_222"
-pool_level_sensor_battery_table = "feed_220"
+pool_level_table = "feed_81"
+pool_level_sensor_battery_table = "feed_80"
+pool_level_sensor_humidity_table = "feed_83"
+pool_level_sensor_temp_table = "feed_82"
+
+# Settings for Power & Solar if using Solar Module to manage pump
+power_total_use = "feed_49"
+power_importing = "feed_42"
+power_solar = "feed_46"
+
+# Other temp sensors if you are using them....
+garage_temp_sensor_humidity_table = "feed_69"
+garage_temp_sensor_battery_table = "feed_70"
+garage_temperature_table = "feed_68"
+attic_temp_sensor_battery_table = "feed_74"
+attic_temperature_table = "feed_75"
+
 
 # At what voltage do we consider the pool_level_sensor to have low batteries?
 pool_level_sensor_low_voltage = 2.9
@@ -56,69 +64,60 @@ max_pool_level_sensor_time_delta = 2000
 # How many Pool Level Sensor Timeouts before we do something?
 max_pool_level_sensor_timeouts = 10
 
-## The HIGHER  the Resistance, the LOWER the pool!!!
-# What is the Pool Level Max Resistance before we need to refill the pool?
-pool_resistance_critical_level = 740  # set back to 740 when done testing)
-
-# What is the pool level low resistance when we decide the pool is full enout?
-pool_resistance_ok_level = 710
-
 # How long to allow the pool to fill before sending an error? (in minutes)
-max_pool_fill_time = 120 
+max_pool_fill_time = 130
+
+# How long to fill the pool each time we ask Alexa to fill it...
+alexa_max_pool_fill_time = 60
 
 # What is the table name for our pool resistance?
-pool_resistance_table = "feed_221"
+pool_resistance_table = "feed_56"
 
 ## What is the table name for our pool_running_watts feed?
-pump_running_watts_table = "feed_153"
-
-# Settings for Power & Solar if using Solar Module to manage pump
-power_total_use = "feed_282"
-power_importing = "feed_281"
-power_solar = "feed_280"
+pump_running_watts_table = "feed_24"
 
 ## What is the table name for our pool_filter_psi?
-pool_filter_psi_table = "feed_273"
-pool_filter_max_psi = 40
-pool_filter_max_psi_reset = 15
-
+pool_filter_psi_table = "feed_67"
+pool_filter_max_psi = 20
+pool_filter_max_psi_reset = 15 
 
 #pool acid realert time: How many minutes between low acid alerts do we want?
 pool_acid_alert_max_minutes = 2880
 
 # How many gallons have we used to fill the pool?
-pool_gallons_total = "feed_269"
-
+pool_gallons_total = "feed_65"
 
 ## Do we have a pool water sensor and are we tracking the pool water temp? (Generally user for accurate pH readings).
 temp_probe = "Yes"
-temp_probe_table = "feed_274"
+temp_probe_table = "feed_53"
 
 ## This is for our pH and ORP probes. This is the information needed to store our pH and ORP reading to our
 ## two emoncms servers
 ##
 ## Do we want to use or do we have the pH and ORP probes installed?
-ph_probe = "No"  # Yes or No
-ph_node = "pool_ph"  # This will be the Node_ID that shows up in your inputs if logging to emoncms
+ph_probe = "Yes"  # Yes or No
+ph_node = int(40)
+#ph_node = "pool_ph"  # This will be the Node_ID that shows up in your inputs if logging to emoncms
 ph_probe_baud = 38400
 
-orp_probe = "No"  # Yes or No
-orp_node = "pool_orp"  # This will be the Node_ID that shows up in your inputs if logging to emoncms
-orp_probe_baud = 38400
+orp_probe = "Yes"  # Yes or No
+orp_node = int(41)
+#orp_node = "pool_orp"  # This will be the Node_ID that shows up in your inputs if logging to emoncms
+orp_probe_baud = 9600
 
 ## Do we have multiple emoncms servers and do we want them turned on...?
 # Server 1 Information
-emoncms_server1 = "Yes"
+emoncms_server1 = "No"
 server1 = "emonpi"  # This could be an IP address, localhost, or an FQDN
 emoncmspath1 = "emoncms"  # This is the path, for example if you want http://www.yourdomain.com/emoncms/
 # you would put "emoncms" here.
-apikey1 = "sdfhkjsdhkjhsdkdjfhiuyiodusoidf"
+apikey1 = "api_key_here"
 
 # Server 2 Information
-emoncms_server2 = "Yes"
-server2 = "emoncms.yourdomain.com"
+emoncms_server2 = "No"
+server2 = "phx-emoncms"
 emoncmspath2 = ""
-apikey2 = "sdljkjhfkjsdhfkjshdkfjhkfjhskhfs"
+apikey2 = "api_key_here"
 
 # Used for Sprinkler Bypass
 sprinkler_bypass = "Yes"  # Set this to "No" to disable all sprinkler checks
@@ -127,39 +126,12 @@ sprinkler_type = "Rachio"  # Timer or Rachio - sprinkler_bypass must be set to "
 # If you have a Rachio Sprinkler System, this is the curl request to get the sprinkler status.
 # You must have your own system, you must get your Auth:Bearer ID as well as you device ID
 # from Rachio in order to use this subprocess.
-rachio_url = 'curl -s -X  GET -H "Content-Type: application/json" -H "Authorization: Bearer diudofisud7-uggdf-4deew-a33ewd-sdfdsfsfsfsd" https://api.rach.io/1/public/device/sfkjshdfkjhsdkfjh-sofiusoiufois-skjdfhksdj/current_schedule'
-
-############################################################################################################################
-########## Runtime checking and maximum fill times - This is for the automatic system, not the manual python check...
-############################################################################################################################
-checktime = 60  # How often (in seconds) do we want to check the pool level? This is how often the pool_level()
-# subroutine will be run which checks the database for the pool level. We will not change the
-# state of the pool fill valve outside of this subroutine, so don't set it to something
-# like 5 hours unless you want a flooded yard!
-
-# Each "checktime cycle" will occur at the interval you specify here.
-
-#######  VERY VERY VERY IMPORTANT ####
-# We now support a watchdog timer. I have set the timer for 90 seconds based on this 60 second checktime. If you change this checktime
-# you MUST MUST MUST change the timer in /etc/systemd/system/pool_control.service otherwise your script will restart every
-# 90 seconds!!!
-
-
-##########################################################################################################################
-maxruntime = 100  # What is the maximum number of checktime cycles you want to let your pool fill? For example
-# if you put 60 seconds above for checktime, and 180 here, it will be 60 seconds * 180 readings
-# which will be a total of three hours. Once this number is hit, if the pool still is not full,
-# we force the pool fill valve closed and notify you (if notification is set on) that there is
-# a problem.
-
-# WARNING * WARNING * WARNING => There is NO time check when running a manual fill. If you fail to shut off your manual
-# fill, your pool will overflow!
-############################################################################################################################
+rachio_url = 'curl -s -X  GET -H "Content-Type: application/json" -H "Authorization: Bearer jjsjdjjds-skksdkkdk-yyey" https://api.rach.io/1/public/device/device_id/current_schedule'
 
 
 ## Set the MAXIMUM idle wattage of your pool pump/equipment. If the reading is over this wattage, the pool will not fill since the
 ## pump would be running. If you do not care if your pump is running while you are filling your pool, set this super high, like 5000.
-max_wattage = 200
+max_wattage = 100
 
 ## Do we have a MightyHat installed with an LCD screen and if so, do we want to output mesages to the LCD screen?
 MightyHat = "True"
